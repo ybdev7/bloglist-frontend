39a4d9f105e21649d8d1058b1e57786aad32c675
ybdev7 <99820567+ybdev7@users.noreply.github.com>
2024-02-23 20:32:18 -0500

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleToggleDetails = (e) => {
+    setShowDetails(!showDetails);
+  };
+  const btnText = showDetails ? "Hide" : "View";
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author}{" "}
+      <button onClick={handleToggleDetails}>{btnText}</button>
+      {showDetails && (
+        <p>
+          Likes {blog.likes} <button>Like</button>
+        </p>
+      )}
+      {showDetails && <p>{blog.user.name}</p>}
+    </div>
+  );
+};
 
 export default Blog;
